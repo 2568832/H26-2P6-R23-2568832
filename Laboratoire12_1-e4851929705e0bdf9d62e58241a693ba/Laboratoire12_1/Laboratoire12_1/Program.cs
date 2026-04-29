00b6853ede17880ac56b3dc7361a938a5607dd44
2568832 <2568832@cegepmontpetit.ca>
2026-04-29 15:07:29 -0400

    Uplode visio

---
+++ Laboratoire12_1-e4851929705e0bdf9d62e58241a693ba/Laboratoire12_1/Laboratoire12_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Modeles;
+
+namespace Laboratoire12_1
+{
+    internal class Program
+    {
+        // TODO 3: Créer les 3 fonctions d'illustration ici
+        static void AfficherAireIllustration1()
+        {
+           
+        }
+        static void AfficherAireIllustration2()
+        {
+        }
+        static void AfficherAireIllustration3()
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            // Exercice 1 (pas dans le code mais visio)
+
+            // Exercice 2
+            // TODO 1: Dans la librairie Modeles, créer les classes de formes nécessaires à partir
+            // de la classe de base Forme
+
+            // Exercice 3
+            // TODO 2: Compléter la classe Caculateur
+
+            // TODO 4: Appeler les 3 fonctions d'illustration ici et afficher l'aire totale pour chacune à la console.
+        
+        }
+    }
+}
---
+++ Laboratoire12_1-e4851929705e0bdf9d62e58241a693ba/Laboratoire12_1/Modeles/Calculateur.cs
@@ -0,0 +1,30 @@
+﻿namespace Modeles
+{
+    public class Calculateur
+    {
+        private List<Forme> polygones;
+        public Calculateur() 
+        { 
+            polygones = new List<Forme>();
+        }
+
+        public void AppliquerForme( Forme polygone )
+        {
+            polygones.Add( polygone );
+        }
+
+        public double AireTotale
+        {
+            get
+            {
+                // 1. Déclarer une somme
+
+                // 2. Pour tous les polygones dans la liste
+                //      a. Obtenir l'aire
+                //      b. Appliquer la bonne opération et mettre à jour la somme
+                // 3. Retourner la somme
+                return 0f;
+            }
+        }
+    }
+}
