c6dd6de70c7931d6dc985e8dc7037728bce6a4f2
2568832 <2568832@cegepmontpetit.ca>
2026-05-04 14:25:31 -0400

    quoincé a l'exercice 4

--- Laboratoire12_2-EX3/Laboratoire12_2/Modeles/CarteReguliere.cs
+++ Laboratoire12_2-EX3/Laboratoire12_2/Modeles/CarteReguliere.cs
@@ -1,7 +1,80 @@
 ﻿namespace Modeles
 {
-    public class CarteReguliere
+    public class CarteReguliere : Carte
     {
-       // TODO
+        // TODO
+        private double mTauxIntérêt;
+
+        public double TauxIntérêt
+        {
+            get
+            {
+                return mTauxIntérêt;
+            }
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("TauxIntérêt");
+                mTauxIntérêt = value;
+            }
+        }
+
+        private double mLimite;
+        public double Limite
+        {
+            get
+            {
+                return mLimite;
+            }
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("Limite");
+                mLimite = value;
+            }
+        }
+
+
+        public CarteReguliere(double pLimite, double pTauxIntérêt) : base(pLimite,pTauxIntérêt)
+        { 
+           Limite = pLimite;
+            TauxIntérêt = pTauxIntérêt;
+
+            
+
+        }
+
+        public override string Avantage {
+            get { return "limite annuelle du Taux d'intérêt"; } 
+        }
+        public override void AppliquerIntérêt()
+        {
+            Limite = Limite;
+        }
+
+        public override bool Emprunter(double pMontant)
+        {
+            if ((TauxIntérêt -= pMontant) <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+
+        }
+        public override bool Rembourser(double pMontant)
+        {
+            if ((TauxIntérêt += pMontant) >= Limite)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
